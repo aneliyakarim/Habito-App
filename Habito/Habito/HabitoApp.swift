186b5639300616d163b533dadcaa30913d7bd020
Aneliya <aneliya_karim@yahoo.co.uk>
2023-05-18 15:13:49 +0200

    init

---
+++ Habito/Habito/HabitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitoApp.swift
+//  Habito
+//
+//  Created by Aneliya Mukhamedkarimova on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
