44f3d391765a87e3761a73f45d890458bd30e535
Aneliya <aneliya_karim@yahoo.co.uk>
2023-06-12 15:54:59 +0200

    Intro Text is added

--- Habito/Habito/Cards/IntroTextView.swift
+++ Habito/Habito/Cards/IntroTextView.swift
@@ -12,7 +12,21 @@ struct IntroTextView: View {
     
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Hello Parent, welcome on board!")
+                .font(.headline)
+            Text("""
+                 We're happy to help you build a bonding habits development experience with your child. 😍
+                 
+                 Quick help on how to do it in the Habito app:
+                 ✅ During the day, record your child's daily habits by adding the cards to an Entry: pick up toys, recycle the trash, or add photos of any success, even a small one.🏆🥳
+                 
+                 ✅ Before going to bed, reflect with your child on what went well and what needs improvement, and reward activities with stars. You can exchange them for something physical like ice cream, toys, or fun experiences later. 🍭🍦🍪🪁🎢
+                 
+                 ✅ Use the Doodle card as a rewarding activity but monitor screen time. ☝️
+                 
+                 Have a lovely storytelling of your child's growth. 😍
+                 """)
+            
         }
         .frame(maxWidth: .infinity, alignment: .topLeading)
         .padding()
